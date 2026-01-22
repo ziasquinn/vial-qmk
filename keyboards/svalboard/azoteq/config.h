bafa629e1f37ffc53c6b4f8f37642cca849474f0
ziasquinn <56662076+ziasquinn@users.noreply.github.com>
2026-01-21 20:14:51 -0500

    Adjust TAP_TIME and HOLD_TIME for touchpad settings
    Increased HOLD_TIME from aggressive 20ms, to more forgiving 150ms. Since TAP_TIME (when default/without defining), is 150ms, also decrease this appropriately. Before, they were confusing each other.

--- keyboards/svalboard/azoteq/config.h
+++ keyboards/svalboard/azoteq/config.h
@@ -8,6 +8,13 @@
 #define AZOTEQ_IQS5XX_TIMEOUT_MS 10
 #define AZOTEQ_IQS5XX_PRESS_AND_HOLD_ENABLE true
 
+#define AZOTEQ_IQS5XX_TAP_TIME 75
+// TAP_TIME default is 150ms
+#define AZOTEQ_IQS5XX_HOLD_TIME 150 
+// HOLD_TIME default is 300ms
+// Here, we decrease the TAP_TIME to account for the reduced in HOLD_TIME so they aren't confused with each other.
+// HOLD_TIME being reduced from 300ms to 150ms decreases the required 'wait' time to activate dragging/selecting on a touchpad, less sensitive than 20ms, but not tedious like 300ms. 
+
 //#define POINTING_DEVICE_MOTION_PIN GP18
 
 #define SPLIT_POINTING_ENABLE
